361bc7d68c1b5648fda5d7653dc5aa25de645b9e
Ryota Ikezawa <pavegy@gmail.com>
2019-09-04 22:45:20 +0900

    :recycle: Improve root command

--- command/root.go
+++ command/root.go
@@ -4,24 +4,58 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var configFile string
 
 const (
 	failedExecution int = 1
 )
 
-var rootCommand = &cobra.Command{
-	Use:   "goura",
-	Short: "goura is a API client of Oura Cloud",
-	Long:  "goura is a Unofficial API client of Oura Cloud written in Go.\nComplete documentation is available at https://github.com/paveg/goura",
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+func NewCommandRoot() *cobra.Command {
+	command := &cobra.Command{
+		Use:   "goura",
+		Short: "goura is a API client of Oura Cloud",
+		Long:  "goura is a Unofficial API client of Oura Cloud written in Go.\nComplete documentation is available at https://github.com/paveg/goura",
+	}
+	cobra.OnInitialize(initConfig)
+
+	command.PersistentFlags().StringVar(&configFile, "config", "", "config file (default is $HOME/.goura.yaml)")
+
+	command.AddCommand(NewVersionCommand())
+	return command
+}
+
+func initConfig() {
+	if configFile != "" {
+		viper.SetConfigFile(configFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(failedExecution)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".goura")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Printf("Using config file: %#v", viper.ConfigFileUsed())
+	}
 }
 
 func Execute() {
-	if err := rootCommand.Execute(); err != nil {
-		fmt.Println(err)
+	command := NewCommandRoot()
+	command.SetOut(os.Stdout)
+	if err := command.Execute(); err != nil {
+		command.SetOut(os.Stderr)
+		command.Println(err)
 		os.Exit(failedExecution)
 	}
 }
--- command/version.go
+++ command/version.go
@@ -12,15 +12,18 @@ const (
 	patchVersion int = 0
 )
 
-var versionCommand = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of goura",
-	Long:  `All software has versions. This is goura's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("goura API client of oura cloud v%v.%v.%v", majorVersion, minorVersion, patchVersion)
-	},
+func NewVersionCommand() *cobra.Command {
+	command := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of goura",
+		Long:  `All software has versions. This is goura's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("goura API client of oura cloud v%v.%v.%v", majorVersion, minorVersion, patchVersion)
+		},
+	}
+
+	return command
 }
 
 func init() {
-	rootCommand.AddCommand(versionCommand)
 }

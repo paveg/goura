cdd630999bcf41bc5acc69a80c9b10a874f3be3a
Ryota Ikezawa <pavegy@gmail.com>
2019-09-04 21:48:32 +0900

    :sparkles: Add version command

---
+++ command/version.go
@@ -0,0 +1,26 @@
+package command
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	majorVersion int = 1
+	minorVersion int = 0
+	patchVersion int = 0
+)
+
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of goura",
+	Long:  `All software has versions. This is goura's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("goura API client of oura cloud v%v.%v.%v", majorVersion, minorVersion, patchVersion)
+	},
+}
+
+func init() {
+	rootCommand.AddCommand(versionCommand)
+}
